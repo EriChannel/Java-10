a3be30bd3e5a2eb9c270d1f243a1a95cdf24052e
Lập trình cùng Eri <erichannel96@gmail.com>
2022-04-14 20:35:41 +0700

    buổi 8

---
+++ Lesson_08/product_demo/src/Main.java
@@ -0,0 +1,11 @@
+//Viết chương trình quản lý sản phẩm của một cửa hàng gồm các thông tin:
+//id, tên sản phẩm , mô tả, số lượng, giá bán, đơn vị tính
+//1 - Xem danh sách sản phẩm
+//2 - Tìm sản phẩm theo tên
+//3 - Tìm sản phẩm theo id:
+// 3.1: Xóa sản phẩm
+// 3.2: Cập nhật số lượng sản phẩm
+
+public class Main {
+
+}
